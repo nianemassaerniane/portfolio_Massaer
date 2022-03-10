5a3ec1e4f8b98682647178e99ec7ef72e56a9c35
nianemassaerniane <nianemassaerniane@gmail.com>
2022-03-10 01:54:54 +0100

    version 2.7 adaptation 1 début footer bouton up  média-phone

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+
+document.querySelector('.btn').addEventListener('click', function(){
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
